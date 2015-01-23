d012279f7888273f6bc9e128332de57c81bbd483
jmw218 <jmw218@lehigh.edu>
2015-01-23 15:44:01 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+/////////////////////////////////
+//Joshua Whitton
+//Demo 0
+//Hello World Java Program
+//
+// First compile: javac HelloWorld.java
+// then run it: java HelloWorld
+//
+
+//Defines the class for the program
+public class HelloWorld{
+    //This adds the main method for the program to run
+    public static void main(String[] args) {
+        
+        //this will print out the strings when the program is run
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("#                    #");
+        System.out.println("# Hello World 2014!! #");
+        System.out.println("#                    #");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("Test");
+    }
+}
