6893da9b11a9565644acf3071671f6aa4cf95c27
jmw218 <jmw218@lehigh.edu>
2015-01-26 19:23:16 +0000

    Welcome Class homework 01 turn in

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+/////////////////////////////////
+//Joshua Whitton
+//Homework 01
+//Welcome Class homework 01
+//
+
+//Defines the program's class and stuff
+public class WelcomeClass{
+    //Creates the main method that the program will run by
+    public static void main(String[] args) {
+        
+        //A set of commands to use when the program is run
+        //Will print out the Strings written here
+        System.out.println("  -----------       ");
+        System.out.println("  | WELCOME |       ");
+        System.out.println("  -----------       ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-J--M--W--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("During christmas, I visited my aunt and uncle in New Jersey.");
+    }
+}
